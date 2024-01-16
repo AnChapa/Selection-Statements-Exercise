89ec985eb7d06da9a1e294e236bcaa080e180ffb
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-16 09:48:17 -0600

    Exercise2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             //Exercise1
+
             var r = new Random();
             var favNumber = r.Next(1, 100);
             var upper = 100;
@@ -27,9 +28,34 @@
                 }
                 else
                 {
-                    Console.WriteLine($"The number {userInput} is correc!");
+                    Console.WriteLine($"The number {userInput} is correct!");
                     found = true;
                 }
+            }
+
+            //Exercise2
+
+            Console.WriteLine("What is your favorite school subject?");
+            string userClass = Console.ReadLine();
+
+            switch(userClass.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is fun.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is intresting.");
+                    break; 
+                case "history":
+                    Console.WriteLine("History is relaxing.");
+                    break;
+                case "gym":
+                case "pe":
+                    Console.WriteLine("Dodgeball is competative.");
+                    break;
+                default:
+                    Console.WriteLine($"{userClass} is a cool class.");
+                    break;
             }
 
         }
