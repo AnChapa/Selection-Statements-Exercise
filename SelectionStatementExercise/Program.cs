0108d198d3210104fcb280da888d32b0eb29f1aa
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-16 09:28:15 -0600

    Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise1
+            var r = new Random();
+            var favNumber = r.Next(1, 100);
+            var upper = 100;
+            var lower = 1;
+            bool found = false;
+
+            while (found == false)
+            {
+                Console.WriteLine($"Guess the number between {lower} and {upper}: ");
+                var userInput = int.Parse(Console.ReadLine());
+                if(userInput > favNumber )
+                {
+                    Console.WriteLine("The number is too high.\n");
+                    upper = userInput;
+                }
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("The number is too low.\n");
+                    lower = userInput;
+                }
+                else
+                {
+                    Console.WriteLine($"The number {userInput} is correc!");
+                    found = true;
+                }
+            }
+
         }
     }
 }
